99ac16271a6f2b3702ea25f90c3f9ce5cd9a1716
Taiki Sugawara <buzz.taiki@gmail.com>
2012-08-04 03:42:20 +0900

    Change test to call method.

--- src/test/java/com/github/buzztaiki/jenova/AnnotationProcessorTest.java
+++ src/test/java/com/github/buzztaiki/jenova/AnnotationProcessorTest.java
@@ -20,27 +20,50 @@
  */
 package com.github.buzztaiki.jenova;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Arrays;
+import java.util.List;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 import org.junit.Test;
 
 public class AnnotationProcessorTest {
+    @SuppressWarnings("unchecked")
     @Test public void test() throws Exception {
+        assertThat(compile("input/Example.java"), is(true));
+
+        URLClassLoader loader = new URLClassLoader(new URL[]{res("input/")});
+        Class<?> exampleClass = loader.loadClass("Example");
+        Object exampleObj = exampleClass.newInstance();
+        assertThat(
+            (List<String>)exampleClass.getMethod("transform", List.class).invoke(
+                exampleObj,
+                Arrays.<Integer>asList(1, 2, 3)),
+            is(Arrays.<String>asList("2", "4", "6")));
+    }
+
+    private boolean compile(String path) throws Exception {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         DiagnosticCollector<JavaFileObject> diags = new DiagnosticCollector<JavaFileObject>();
         StandardJavaFileManager fm = compiler.getStandardFileManager(diags, null, null);
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
         JavaCompiler.CompilationTask task = compiler.getTask(
             null, fm, null, null, null,
-            fm.getJavaFileObjects(new File(cl.getResource("input/Example.java").toURI())));
+            fm.getJavaFileObjects(new File(res(path).toURI())));
         task.setProcessors(Arrays.asList(new AnnotationProcessor()));
-        assertTrue(task.call());
+        return task.call();
+    }
+
+    private URL res(String path) {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        return loader.getResource(path);
     }
 }
--- src/test/resources/input/Example.java
+++ src/test/resources/input/Example.java
@@ -1,10 +1,12 @@
 import com.github.buzztaiki.jenova.Jenova;
+import com.google.common.collect.Lists;
+import java.util.List;
 
 @Jenova
 public class Example {
-    public static void main(String[] args) throws Exception {
-        new fn<Integer, String>(){{
-            return Integer.toString(_);
-        }};
+    public List<String> transform(List<Integer> l) {
+        return Lists.transform(l, new fn<Integer, String>() {{
+            return Integer.toString(_ * 2);
+        }});
     }
 }
