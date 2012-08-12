01a3d830d4fe7bba2493742d4d351f47925860bc
Taiki Sugawara <buzz.taiki@gmail.com>
2012-08-12 23:40:34 +0900

    Use LambdaTransformer.

--- src/main/java/com/github/buzztaiki/jenova/AnnotationProcessor.java
+++ src/main/java/com/github/buzztaiki/jenova/AnnotationProcessor.java
@@ -22,17 +22,13 @@ package com.github.buzztaiki.jenova;
 
 import com.sun.source.util.TreePath;
 import com.sun.source.util.Trees;
-import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.model.JavacElements;
-import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.TreeInfo;
-import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.Name;
+import java.util.HashMap;
+import java.util.Map;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -46,48 +42,39 @@ import javax.tools.JavaFileObject;
 @SupportedAnnotationTypes("com.github.buzztaiki.jenova.Jenova")
 public class AnnotationProcessor extends AbstractProcessor {
     private Context context;
-    private TreeMaker maker;
-    private JavacElements elems;
-    private TreeInfo info;
 
     @Override
     public void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         if (!(processingEnv instanceof JavacProcessingEnvironment)) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Jenova requires javac v1.6 or greater.");
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Jenova requires JavacProcessingEnvironment.");
             return;
         }
         context = ((JavacProcessingEnvironment)processingEnv).getContext();
-        if (context != null) {
-            maker = TreeMaker.instance(context);
-            elems = JavacElements.instance(context);
-            info = TreeInfo.instance(context);
-        }
     }
 
     @Override
     public boolean process (Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (context == null) return false;
 
-        for (Element elem : roundEnv.getRootElements()) {
+        for (Element elem : roundEnv.getElementsAnnotatedWith(Jenova.class)) {
+            Jenova jenova = elem.getAnnotation(Jenova.class);
+            Map<String, InterfaceMethod> ifMethods = ifMethods(jenova.value());
+            if (ifMethods.isEmpty()) continue;
+
             final JCTree.JCCompilationUnit unit = toUnit(elem);
             if (unit == null) continue;
             if (unit.sourcefile.getKind() != JavaFileObject.Kind.SOURCE) continue;
+
+            final LambdaTransformer transformer = new LambdaTransformer(context, ifMethods);
             unit.accept(new TreeTranslator() {
                 @Override public void visitNewClass(JCTree.JCNewClass tree) {
                     super.visitNewClass(tree);
-                    if (info.name(tree.getIdentifier()).toString().equals("fn")) {
-                        result = translateFn(tree);
-                    }
+                    result = transformer.transform(tree);
                 }
             });
         }
         return false;
-    }
-
-    private void printTree(JCTree tree) {
-        if (tree == null) return;
-        System.out.format("##%s:%s%n", tree.getClass(), tree);
     }
 
     private JCTree.JCCompilationUnit toUnit(Element element) {
@@ -95,60 +82,14 @@ public class AnnotationProcessor extends AbstractProcessor {
         return (path == null) ? null : (JCTree.JCCompilationUnit)path.getCompilationUnit();
     }
 
-    private JCTree translateFn(JCTree.JCNewClass fn) {
-        List<JCTree.JCExpression> typeArgs = appliedTypes(fn.getIdentifier());
-        JCTree.JCClassDecl body = fn.getClassBody();
-        List<JCTree> members = body.getMembers();
-        JCTree.JCBlock initBlock = (JCTree.JCBlock)(members.get(0));
-        JCTree.JCMethodDecl method = maker.MethodDef(
-            maker.Modifiers(Flags.PUBLIC),
-            elems.getName("apply"),
-            typeArgs.get(1),
-            List.<JCTree.JCTypeParameter>nil(),
-            List.of(arg("_", typeArgs.get(0))),
-            List.<JCTree.JCExpression>nil(),
-            initBlock,
-            null);
-        return newClass(
-            fn, "com.google.common.base.Function",
-            classBody(body, method));
-    }
-
-    private JCTree.JCExpression ident(JCTree.JCExpression orig, String name) {
-        JCTree.JCExpression ident = maker.QualIdent(elems.getTypeElement(name));
-        List<JCTree.JCExpression> typeArgs = appliedTypes(orig);
-        if (typeArgs != null) return maker.TypeApply(ident, typeArgs);
-        return ident;
-    }
-
-    private List<JCTree.JCExpression> appliedTypes(JCTree.JCExpression ident) {
-        if (ident instanceof JCTree.JCTypeApply) {
-            JCTree.JCTypeApply ta = (JCTree.JCTypeApply)ident;
-            return ta.getTypeArguments();
+    private Map<String, InterfaceMethod> ifMethods(Lambda[] lambdas) {
+        JavacElements elems = JavacElements.instance(context);
+        Map<String, InterfaceMethod> res = new HashMap<String, InterfaceMethod>();
+        for (Lambda lambda : lambdas) {
+            res.put(lambda.name(), new InterfaceMethod(
+                context,
+                elems.getTypeElement(lambda.type().getCanonicalName())));
         }
-        return null;
-    }
-
-    private JCTree.JCVariableDecl arg(String name,  JCTree.JCExpression vartype) {
-        return maker.VarDef(maker.Modifiers(0), elems.getName(name), vartype, null);
-    }
-
-    private JCTree.JCClassDecl classBody(JCTree.JCClassDecl orig, JCTree.JCMethodDecl method) {
-        return maker.ClassDef(
-            orig.getModifiers(),
-            orig.getSimpleName(),
-            orig.getTypeParameters(),
-            orig.getExtendsClause(), // TODO: compile failed when java7
-            orig.getImplementsClause(),
-            List.<JCTree>of(method));
-    }
-
-    private JCTree.JCNewClass newClass(JCTree.JCNewClass orig, String name, JCTree.JCClassDecl classBody) {
-        return maker.NewClass(
-            orig.getEnclosingExpression(),
-            orig.getTypeArguments(),
-            ident(orig.getIdentifier(), name),
-            orig.getArguments(),
-            classBody);
+        return res;
     }
 }
